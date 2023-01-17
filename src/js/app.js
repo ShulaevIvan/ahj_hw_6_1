72e0741ebc071dc0c5ba67d2a46bc9a752d0e075
Shulaev Ivan <mail@ntdservice.ru>
2023-01-18 02:26:16 +0300

    localstorage clear add

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,11 @@ window.addEventListener('DOMContentLoaded', () => {
   const cardManager = new CardManager('.container');
   const allBtn = document.querySelectorAll('.add-card-container');
   const storage = JSON.parse(localStorage.getItem('cards'));
-  if (storage) cardManager.restoreCardsStorage(storage);
+  if (storage) {
+    cardManager.restoreCardsStorage(storage);
+  } else {
+    localStorage.clear();
+  }
 
   allBtn.forEach((item) => {
     item.addEventListener('click', (e) => {
